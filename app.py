ee229b85076d58206640772539a3d40a31143c62
czaraugustocoder <cesaraugustoandradeferreira@gmail.com>
2022-08-10 13:18:13 +0000

    Adcionado um código html

--- app.py
+++ app.py
@@ -9,3 +9,15 @@ def Hello():
 @app.route("/contatos")
 def contato():
     return "Cesar Augusto Andrade Ferreira"
+
+@app.route("/html")
+def html():
+    return """<html>
+        <head>
+            <title> Agenda Do Governo </title>
+        </head>
+        <body>
+            <h1>Lista de Contatos das Entidades Administrativas do Estado do Amazonas</h1>
+        </body>
+    </html>
+    """
